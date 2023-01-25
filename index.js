3892d41df91760fd87e7859caa98cee5828c17cb
Mst-Akhi-khatun01~ <akhikhatun7732@gmail.com>
2023-01-25 17:07:54 +0600

    some changes added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 var jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
